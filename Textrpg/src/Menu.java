2565c8217ec9e954e13030fee1b2eb64e9b4f888
Msallam2 <msallam.al-jwabreh@hebk.de>
2021-11-24 11:39:19 +0100

    Changes

--- Textrpg/src/Menu.java
+++ Textrpg/src/Menu.java
@@ -16,15 +16,17 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
+import javax.swing.text.AttributeSet.ColorAttribute;
+
 import org.w3c.dom.css.RGBColor;
 
 public class Menu implements ActionListener {
 
     JLabel tn;
     static JLabel l;
-    static JButton Option1;
-    static JButton Option2;
-    static JButton Option3;
+    static JButton option1;
+    static JButton option2;
+    static JButton option3;
 
     JFrame frame;
     static JProgressBar button1;
@@ -32,6 +34,7 @@ public class Menu implements ActionListener {
     JPanel panel1, panel2;
 
     JFrame frameLoading;
+   static JLabel optionsmenu;
     Font titelgröße = new Font("Times new Roman", Font.PLAIN, 80);
     Font startschrift = new Font("Times new Roman", Font.PLAIN, 60);
     JButton startname;
@@ -74,7 +77,7 @@ public class Menu implements ActionListener {
         startname.setForeground(Color.WHITE);
         // startname.setFocusable(false);
         startname.addActionListener(this);
-        startname.setActionCommand("startname ");
+        startname.setActionCommand("startname");
 
         button1 = new JProgressBar();
         button1.setBorderPainted(false);
@@ -95,47 +98,56 @@ public class Menu implements ActionListener {
         l.setFocusable(false);
 
 
-        Option1 = new JButton("   Start   ");
-        Option1.setForeground(Color.white);
-        Option1.setFont(new Font("Times new Roman", Font.PLAIN, 30));
-        Option1.setBounds(500, 300, 350, 350);
-        Option1.setSize(300, 70);
-        Option1.setBackground(Color.black);
-        Option1.setVisible(false);
-        Option1.setFocusable(false);
-        Option1.addActionListener(this);
-        Option1.setActionCommand("Option1");
+        option1 = new JButton("   Start   ");
+        option1.setForeground(Color.white);
+        option1.setFont(new Font("Times new Roman", Font.PLAIN, 30));
+        option1.setBounds(500, 300, 350, 350);
+        option1.setSize(300, 70);
+        option1.setBackground(Color.black);
+        option1.setVisible(false);
+        option1.setFocusable(false);
+        option1.addActionListener(this);
+        option1.setActionCommand("Option1");
 
-        Option2 = new JButton("   Options   ");
-        Option2.setForeground(Color.white);
-        Option2.setFont(new Font("Times new Roman", Font.PLAIN, 30));
-        Option2.setBounds(500, 400, 350, 350);
-        Option2.setSize(300, 70);
-        Option2.setBackground(Color.black);
-        Option2.setVisible(false);
-        Option2.setFocusable(false);
-        Option2.addActionListener(this);
-        Option2.setActionCommand("Option2");
+        option2 = new JButton("   Options   ");
+        option2.setForeground(Color.white);
+        option2.setFont(new Font("Times new Roman", Font.PLAIN, 30));
+        option2.setBounds(500, 400, 350, 350);
+        option2.setSize(300, 70);
+        option2.setBackground(Color.black);
+        option2.setVisible(false);
+        option2.setFocusable(false);
+        option2.addActionListener(this);
+        option2.setActionCommand("option2");
 
-        Option3 = new JButton("   Exit   ");
-        Option3.setForeground(Color.white);
-        Option3.setFont(new Font("Times new Roman", Font.PLAIN, 30));
-        Option3.setBounds(500, 500, 350, 350);
-        Option3.setSize(300, 70);
-        Option3.setBackground(Color.black);
-        Option3.setVisible(false);
-        Option3.setFocusable(false);
-        Option3.addActionListener(this);
-        Option3.setActionCommand("Option3");
+        option3 = new JButton("   Exit   ");
+        option3.setForeground(Color.white);
+        option3.setFont(new Font("Times new Roman", Font.PLAIN, 30));
+        option3.setBounds(500, 500, 350, 350);
+        option3.setSize(300, 70);
+        option3.setBackground(Color.black);
+        option3.setVisible(false);
+        option3.setFocusable(false);
+        option3.addActionListener(this);
+        option3.setActionCommand("option3");
+
+        optionsmenu= new JLabel();
+        optionsmenu.setBounds(500,500,600,600);
+        optionsmenu.setVisible(false);
+        optionsmenu.setBackground(Color.red);
+        optionsmenu.setFocusable(false);
+optionsmenu.setForeground(Color.white);
        //   Option1.setBackground(Color.white);
       //  Option1 = startname.clone
         frame.add(panel2);
         panel1.add(tn);
         frame.add(l);
-        frame.add(Option1);
-        frame.add(Option2);
-        frame.add(Option3);
+        frame.add(optionsmenu);
+        frame.add(option1);
+        frame.add(option2);
+        frame.add(option3);
         frame.add(panel1);
+      //  tn.add(optionsmenu);
         frame.setVisible(true);
 
         while (l.isVisible() == false) {
@@ -170,15 +182,15 @@ public class Menu implements ActionListener {
 
         }
 
-        if (e.getActionCommand()=="Option2"){
-            System.out.println("Option2");
-
+        if (e.getActionCommand()=="option2"){
+            System.out.println("option2");
+optionsmenu.setVisible(true);
         }
 
-        if (e.getActionCommand()=="Option3"){
-            System.out.println("Option3");
+        if (e.getActionCommand()=="option3"){
+            System.out.println("option3");
             System.out.println("Dsalkgbw");
-
+frame.setVisible(false);
         }
     }
 
@@ -234,32 +246,36 @@ public class Menu implements ActionListener {
                 Thread.sleep(3);
                 l.setBounds(540, 30 - i - 5, 500, 500);
                 if (i >= 100) {
-                    Thread.sleep(500);
-                    l.setText("               D");
                     Thread.sleep(80);
-                    l.setText("              Di");
-                    Thread.sleep(80);
-                    l.setText("             Dia");
-                    Thread.sleep(80);
-                    l.setText("            Dial");
-                    Thread.sleep(80);
-                    l.setText("           Dialo");
-                    Thread.sleep(80);
-                    l.setText("          Dialog");
-                    Thread.sleep(80);
-                    Option1.setVisible(true);
+                    option1.setVisible(true);
                     Thread.sleep(200);
-                    Option2.setVisible(true);
+                    option2.setVisible(true);
                     Thread.sleep(200);
-                    Option3.setVisible(true);
+                    option3.setVisible(true);
 
+                    String message;
+
+                    message = "        The Day";
+System.out.println(message.toCharArray());
+//System.out.println(message.length());
+Thread.sleep(1000);
+l.setText("");
+                    for (Byte m1 = 0;m1<message.length();m1++){
+                    System.out.print(message.charAt(m1));
+                   // Option1.setText(message.charAt(m1));
+                    Thread.sleep(28);
                     
+                    l.setText(l.getText() + message.charAt(m1));
+                    }
 
                     for (n1 = 0;; n1++){
                        // System.out.println(t.charAt(0-n1));
                         System.out.println(t.copyValueOf(null, 1, n1));
                        // l.setText(t.charAt(0-n1));
                         }
+
+                        
+
                 }
 
             }
@@ -296,7 +312,7 @@ public class Menu implements ActionListener {
             System.out.print("D");
         }
 
-        if (((EventObject) e).getSource() == this.Option1) {
+        if (((EventObject) e).getSource() == this.option1) { 
           //  Loading();
             System.out.print("Start");
         }
